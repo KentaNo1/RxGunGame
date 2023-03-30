2a03367ad6e19270f6ac1e1e6e042cf93617d3ca
Mees Verhelpen <mees.verhelpen@gmail.com>
2023-03-30 16:04:24 +0200

    Chat Suggestions

--- commands/cl_commands.lua
+++ commands/cl_commands.lua
@@ -2,6 +2,11 @@
 BY Rejox#7975 © RX
 --]]
 
+TriggerEvent('chat:addSuggestions', {
+    { name = "/" .. Config.LeaveCommand, help = "Leave GunGame" },
+    { name = "/" .. Config.ResetLeaderboardCommand, help = "Reset GunGame Leaderboard (Admin)" },
+})
+
 RegisterCommand(Config.LeaveCommand, function (source, args, raw)
     TriggerServerEvent("sv_game:leaveGunGame")
 end)
