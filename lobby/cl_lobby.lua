041f53be843e70d155674a781241f983f375da47
Mees Verhelpen <mees.verhelpen@gmail.com>
2023-03-31 19:26:26 +0200

    Lobby in thread

--- lobby/cl_lobby.lua
+++ lobby/cl_lobby.lua
@@ -4,19 +4,19 @@ BY Rejox#7975 © RX
 
 local joinNPC = nil
 
-if Config.JoinLobby.Blip.Active then
-    local lobbyBlip = AddBlipForCoord(Config.JoinLobby.Coords.x, Config.JoinLobby.Coords.y, Config.JoinLobby.Coords.z)
-    SetBlipSprite(lobbyBlip, Config.JoinLobby.Blip.Sprite)
-    SetBlipDisplay(lobbyBlip, 4)
-    SetBlipScale(lobbyBlip, Config.JoinLobby.Blip.Scale)
-    SetBlipColour(lobbyBlip, Config.JoinLobby.Blip.Color)
-    SetBlipAsShortRange(lobbyBlip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(Config.JoinLobby.Blip.Label)
-    EndTextCommandSetBlipName(lobbyBlip)
-end
-
 CreateThread(function()
+    if Config.JoinLobby.Blip.Active then
+        local lobbyBlip = AddBlipForCoord(Config.JoinLobby.Coords.x, Config.JoinLobby.Coords.y, Config.JoinLobby.Coords.z)
+        SetBlipSprite(lobbyBlip, Config.JoinLobby.Blip.Sprite)
+        SetBlipDisplay(lobbyBlip, 4)
+        SetBlipScale(lobbyBlip, Config.JoinLobby.Blip.Scale)
+        SetBlipColour(lobbyBlip, Config.JoinLobby.Blip.Color)
+        SetBlipAsShortRange(lobbyBlip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(Config.JoinLobby.Blip.Label)
+        EndTextCommandSetBlipName(lobbyBlip)
+    end
+
     if Config.JoinLobby.NPC.Active then
         if not joinNPC or not DoesEntityExist(joinNPC) then
             local joinNPC = Config.JoinLobby.NPC
